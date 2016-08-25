4b3db9d8a6d8de2484b9c730e65907078cb0cbee
Pablo Anglat <panglat@gmail.com>
2016-08-25 16:38:18 -0300

    DIY-18 Epic : Mobile Settings tool Modify the controller to get inputs data Save the latitude, longitude and radius every time the user presses Save button Show all the data in a table

--- app/areaSpecificationForm/areaSpecificationForm.component.ts
+++ app/areaSpecificationForm/areaSpecificationForm.component.ts
@@ -1,13 +1,24 @@
 import {Component} from 'angular2/core';
 
+export class Location {
+  constructor(public latitude: double, public longitude: double, public radius: double) { }
+}
+
 @Component({
     selector: 'areaSpecificationForm',
     templateUrl: 'app/areaSpecificationForm/areaSpecificationForm.component.html',
     styleUrls: ['app/areaSpecificationForm/areaSpecificationForm.component.css']
 })
 export class AreaSpecificationForm {
+	_location : Location;
+	_locations : Location[];
+	constructor() {
+		this._location = new Location(0.0, 0.0, 0.0);
+		this._locations = [];
+	}
 
-    onSubmit(areaSpecification){
-        console.log("test");
+    save() {
+		this._locations.push(this._location);
+		this._location = new Location(0.0, 0.0, 0.0);
     }
 }
