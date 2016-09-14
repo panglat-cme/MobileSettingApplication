1a4a9bea7ebdc3b060f8c3e8750c563a0a69b944
panglat <panglat@gotocme.com>
2016-09-14 12:56:41 -0300

    Fix url problem

--- app/constants.ts
+++ app/constants.ts
@@ -1,6 +1,6 @@
 export class Constants {
     public static ERROR_RETRIEVING_LIST = "An error occurred retrieving the list of ";
     public static ERROR_RETRIEVING_MOBILE_SETTINGS = "An error occurred retrieving the settings for ";
-    public static BASE_SERVER_URL = 'http://172.17.1.45:8899/MobileSettings/';
+    public static BASE_SERVER_URL = 'http://intranet.cmeoffshore.com:8899/MobileSettings/';
     public static PER_CATEGORY_NAME = "Per Category";
 }
--- app/services/refineLocation.service.ts
+++ app/services/refineLocation.service.ts
@@ -14,7 +14,7 @@ export class RefineLocationService extends DiyServerService {
      * @returns {Observable<R>}
      */
     getRefineLocations() {
-        return this._http.get('http://172.17.2.223:9000/MobileSettings/SettingCategories?projectSettingId=5')
+        return this._http.get(Constants.BASE_SERVER_URL + 'SettingCategories?projectSettingId=5')
             .map((response: Response) => response.json().data)
             .catch(this.handleResponseError);
     }
